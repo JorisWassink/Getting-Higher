af6433a05a22e1eca75470ac783c11b53cc49e1f
R-Bleach <loljk366@gmail.com>
2024-02-15 12:56:35 +0100

    Ui and fuel added

---
+++ GXPEngine/Ui.cs
@@ -0,0 +1,34 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ui : GameObject
+{
+    EasyDraw fuelBar;
+    Font rowdies;
+    public Ui()
+    {
+        rowdies = Utils.LoadFont("Assets/Rowdies-Regular.ttf", 40);
+        fuelBar = new EasyDraw(250, 60, false);
+        fuelBar.TextFont(rowdies);
+        fuelBar.TextAlign(CenterMode.Min, CenterMode.Center);
+        fuelBar.Fill(0, 255, 0);
+        fuelBar.Text("Fuel: 100");
+        fuelBar.SetXY(10, 10);
+        /*Console.WriteLine(fuelBar);*/
+        AddChild(fuelBar);
+
+
+    }
+    public void SetFuel(float fuelCount)
+    {
+        fuelBar.Text(String.Format("Fuel: " + fuelCount), true);
+    }
+    void Update()
+    {
+    }
+}
